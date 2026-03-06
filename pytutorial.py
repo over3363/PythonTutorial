44f7267bfd2fe4c0d57a914618d18a20833a7d9f
over3363 <over3363@gmail.com>
2026-03-06 10:19:01 -0800

    Membership Operators

--- pytutorial.py
+++ pytutorial.py
@@ -1411,3 +1411,393 @@ print("hello" in text)
 print("z" not in text)
 
 print("-----------------------BITWISE OPERATORS--------------------") # PRINTED FOR OUTPUT CLARITY 
+#BITWISE OPERATORS 
+# Bitwise operators are used to compare (binary) numbers
+"""
+Operator	Name	Description	Example	
+& 	AND	Sets each bit to 1 if both bits are 1	x & y	
+|	OR	Sets each bit to 1 if one of two bits is 1	x | y	
+^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ y	
+~	NOT	Inverts all the bits	~x	
+<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	x << 2	
+>>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	x >> 2
+"""
+
+#&
+print(6 & 3)
+"""
+#The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0:
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+2 = 0000000000000010
+====================
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+# |
+print(6 | 3)
+
+
+
+"""
+The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+7 = 0000000000000111
+====================
+
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+# ^
+# The ^ operator compares each bit and set it to 1 if only one is 1, otherwise it is set to 0
+# 6 = 0110
+# 3 = 0011
+# --------
+# 5 = 0101
+
+print(6 ^ 3)
+#~
+print(~3)
+
+
+
+"""
+The ~ operator inverts each bit (0 becomes 1 and 1 becomes 0).
+
+Inverted 3 becomes -4:
+ 3 = 0000000000000011
+-4 = 1111111111111100
+
+Decimal numbers and their binary values:
+ 4 = 0000000000000100
+ 3 = 0000000000000011
+ 2 = 0000000000000010
+ 1 = 0000000000000001
+ 0 = 0000000000000000
+-1 = 1111111111111111
+-2 = 1111111111111110
+-3 = 1111111111111101
+-4 = 1111111111111100
+"""
+#<<
+print(3 << 2)
+
+
+
+"""
+The << operator inserts the specified number of 0's (in this case 2) from the right and let the same amount of leftmost bits fall off:
+
+If you push 00 in from the left:
+ 3 = 0000000000000011
+becomes
+12 = 0000000000001100
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
+#>>
+print(8 >> 2)
+
+
+
+"""
+The >> operator moves each bit the specified number of times to the right. Empty holes at the left are filled with 0's.
+
+If you move each bit 2 times to the right, 8 becomes 2:
+ 8 = 0000000000001000
+becomes
+ 2 = 0000000000000010
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
+
+# The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0
+print(6 & 3)
+#The binary representation of 6 is 0110
+#The binary representation of 3 is 0011
+#Then the & operator compares the bits and returns 0010, which is 2 in decimal.
+
+# The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0
+# 6 = 0110
+# 3 = 0011
+# --------
+# 2 = 0010
+print(6 & 3)
+
+#The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+print(6 | 3)
+#The binary representation of 6 is 0110
+#The binary representation of 3 is 0011
+#Then the | operator compares the bits and returns 0111, which is 7 in decimal.
+# The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0
+# 6 = 0110
+# 3 = 0011
+# --------
+# 7 = 0111
+print(6 | 3)
+
+#The ^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0:
+print(6 ^ 3)
+#The binary representation of 6 is 0110
+#The binary representation of 3 is 0011
+#Then the ^ operator compares the bits and returns 0101, which is 5 in decimal.
+# The ^ operator compares each bit and set it to 1 if only one is 1, otherwise it is set to 0
+# 6 = 0110
+# 3 = 0011
+# --------
+# 5 = 0101
+print(6 ^ 3)
+
+print("--------------------OPERATOR PRECEDENCE--------------------") # PRINTED FOR OUTPUT CLARITY 
+# OPERATOR PRECEDENCE 
+# Operator precedence describes the order in which operations are performed
+
+# Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first
+print((6 + 3) - (6 + 3))
+
+"""
+Parenthesis have the highest precedence, and need to be evaluated first.
+The calculation above reads 9 - 9 = 0
+"""
+
+# Multiplication * has higher precedence than addition +, and therefore multiplications are evaluated before additions
+print(100 + 5 * 3)
+
+"""
+Multiplication has higher precedence than addition, and needs to be evaluated first.
+The calculation above reads 100 + 15 = 115
+"""
+print("--------------------PRECENDENCE ORDER--------------------") # PRINTED FOR OUTPUT CLARITY 
+#The precedence order is described in the table below, starting with the highest precedence at the top
+"""
+Operator	Description	
+()	Parentheses	
+**	Exponentiation	
++x  -x  ~x	Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	Multiplication, division, floor division, and modulus	
++  -	Addition and subtraction	
+<<  >>	Bitwise left and right shifts	
+&	Bitwise AND	
+^	Bitwise XOR	
+|	Bitwise OR	
+==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+not	Logical NOT	
+and	AND	
+or	OR
+"""
+
+#()
+print((6 + 3) - (6 + 3))
+
+"""
+Parenthesis have the highest precedence, and need to be evaluated first.
+The calculation above reads 9 - 9 = 0
+"""
+#**
+print(100 - 3 ** 3)
+
+"""
+Exponentiation has higher precedence than subtraction, and needs to be evaluated first.
+The calculation above reads 100 - 27 = 73
+"""
+#+x  -x  ~x
+print(100 + ~3)
+
+"""
+Bitwise NOT has higher precedence than addition, and needs to be evaluated first.
+The calculation above reads 100 + -4 = 96
+"""
+#*  /  //  %
+print(100 + 5 * 3)
+
+"""
+Multiplication has higher precedence than addition, and needs to be evaluated first.
+The calculation above reads 100 + 15 = 115
+"""
+#+  -
+print(100 - 5 * 3)
+
+"""
+Subtraction has a lower precedence than multiplication, and we need to calculate the multiplication first.
+The calculation above reads 100 - 15 = 85
+"""
+#<<  >>
+print(8 >> 4 - 2)
+
+"""
+Bitwise right shift has a lower precedence than subtraction, and we need to calculate the subtraction first.
+The calculation above reads 8 >> 2 = 2
+
+More explanation:
+The >> operator moves each bit the specified number of times to the right. Empty holes at the left are filled with 0's.
+
+If you move each bit 2 times to the right, 8 becomes 2:
+ 8 = 0000000000001000
+becomes
+ 2 = 0000000000000010
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
+#&
+print(6 & 2 + 1)
+
+"""
+Bitwise AND has a lower precedence than addition, and we need to calculate the addition first.
+The calculation above reads 6 & 3 = 2
+
+More explanation:
+The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+2 = 0000000000000010
+====================
+
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+#^
+print(6 ^ 2 + 1)
+
+"""
+Bitwise XOR has a lower precedence than addition, and we need to calculate the addition first.
+The calculation above reads 6 ^ 3 = 5
+
+More explanation:
+The ^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+5 = 0000000000000101
+====================
+
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+#|
+print(6 | 2 + 1)
+
+"""
+Bitwise OR has a lower precedence than addition, and we need to calculate the addition first.
+The calculation above reads 6 | 3 = 7
+
+More explanation:
+The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+7 = 0000000000000111
+====================
+
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+#==  !=  >  >=  <  <=  is  is not  in  not in 
+print(5 == 4 + 1)
+
+"""
+The "like" comparison has a lower precedence than addition, and we need to calculate the addition first.
+The calculation above reads 5 == 5 = True
+"""
+#not
+print(not 5 == 5)
+
+"""
+The logical NOT operator has a lower precedence than "like" comparison, and we need to calculate the comparison first.
+The calculation above reads: not True = False
+"""
+#and
+print(1 or 2 and 3)
+
+"""
+The and operator has a higher precedence than or, and we need to calculate the and expression first.
+The calculation above reads: 1 or 3 = 1
+"""
+#or
+print(4 or 5 + 10 or 8)
+
+"""
+The or operator has a lower precedence than addition, and we need to calculate the addition first.
+The calculation above reads: 4 or 15 or 8 = 4
+"""
+
